244e42a0c44483720e3100730f1f56dc730e87b9
Aditya Rana <adityarana2412@Adityas-MacBook-Air.local>
2025-01-19 20:09:04 +0530

    Node-api Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express=require("express")
+const users=require("./MockData.json")
+const app=express()
+const port=8000
+app.get("/users",(req,res)=>{
+    return res.json(users)
+})
+app.listen(port,()=>console.log("server is running"))
