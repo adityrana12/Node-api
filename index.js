aeb209a2b880e289aa3f78c7e0f617abc052eab7
Aditya Rana <adityarana2412@Adityas-MacBook-Air.local>
2025-01-19 20:33:50 +0530

    changes to index file

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const port=8000
 app.get("/users",(req,res)=>{
     return res.json(users)
 })
+app.get("/students",(req,res)=>{
+    return res.json(users)
+})
 app.listen(port,()=>console.log("server is running"))
